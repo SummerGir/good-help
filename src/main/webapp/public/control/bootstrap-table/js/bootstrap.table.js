9ab8f153138866645bd8aa9b50c8efea1828804e
谢世兵 <742152637@qq.com>
2017-10-30 17:31:39 +0800

    自定义表格的列表控件

---
+++ src/main/webapp/public/control/bootstrap-table/js/bootstrap.table.js
@@ -0,0 +1,12 @@
+/**
+ * Created by xiucai on 2017/10/30.
+ */
+//定义一个匿名函数
+(function (){
+    if($.ghTable == undefined){
+        $.ghTable = {};
+    }
+    $.ghTable.list = function(options){
+
+    };
+})();
